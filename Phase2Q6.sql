798c624e9966c86e155b78ad36cae1a52b8d886c
Adam Sinck <absinck+github@grueslayer.com>
2017-03-14 22:01:55 -0700

    move the queries

--- Phase2Q6.sql
+++
@@ -1,9 +0,0 @@
-/*
-All band members and bands that started a tour sometime during, or after, 2016.
-*/
-SELECT person_name, band_name
-FROM person
-WHERE person.band_name IN
-	(SELECT band_name
-	FROM tour
-	WHERE tour_start_date >= '2016-01-01');
