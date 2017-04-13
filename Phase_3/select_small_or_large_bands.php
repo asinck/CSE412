72e3c8e95bdde27e8f62039a61070d60e5d16845
Mikeiscool <mrainsfo@asu.edu>
2017-04-13 14:32:11 -0700

    Select query gets list of small or large bands based on user input

---
+++ Phase_3/select_small_or_large_bands.php
@@ -0,0 +1,38 @@
+<?php 
+
+require "../h.php";
+
+
+//If user enters large they will get a list of all the bands who have more than ten members
+//If user enters small they will get a list of all the bands who have less than ten members
+
+$response = "ERROR: no accetable parameters were set.";
+// build the sql query
+
+// get user input
+if( isset( $_GET['input'] ) ){
+
+	if (strcmp($var1, $var2) !== 0)
+		if(strcmp($_GET['input'], "large") ==0 || strcmp($_GET['input'], "Large") ==0 )
+		{
+			$sql = "SELECT COUNT(person_name)NUMBER_OF_STAFF, band_name AS BAND_NAME
+				FROM staff
+				GROUP BY band_name
+				HAVING COUNT(person_name) > 10;";
+		}
+		else if(strcmp($_GET['input'], "small") ==0 || strcmp($_GET['input'], "Small") ==0 )
+		{
+			$sql = "SELECT COUNT(person_name)NUMBER_OF_STAFF, band_name AS BAND_NAME
+				FROM staff
+				GROUP BY band_name
+				HAVING COUNT(person_name) < 10;";
+		}
+	
+		
+	$response = sql_get_query($sql);
+}
+
+// return response in json
+jr( $response );
+
+?>
